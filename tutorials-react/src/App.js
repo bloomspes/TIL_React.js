1e80b7e32ef05b630b580e82d39e9b6902a2ba24
Spes <bloomspes@gmail.com>
2020-07-28 16:52:42 +0900

    event rendering practice

--- tutorials-react/src/App.js
+++ tutorials-react/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import LifeCycleSample from './LifeCycleSample';
 import ErrorBoundary from './ErrorBoundary';
+import EventPractice from './EventPractice';
 
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
+
+const App = () => {
+  return <EventPractice />;
+};
 
 class App extends Component {
   state = {
