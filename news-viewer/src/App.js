86a87743715040a93b755739ba34f221066f8af1
Spes <bloomspes@gmail.com>
2020-07-30 21:24:25 +0900

    fixed syntax using by useState module

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import NewsList from './components/NewsList';
 import Categories from './components/Categories';
 
+// Category의 상태를 useState로 관리 하도록 코드 바꿈.
 const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
   return (
     <>
-      <Categories />
-      <NewsList />
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
     </>
   );
 };
