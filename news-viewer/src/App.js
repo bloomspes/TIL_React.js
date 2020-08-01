2084b96485b13b0e4abbbd97994133e47e441749
Spes <bloomspes@gmail.com>
2020-08-01 23:05:54 +0900

    react router dom 활용해서 다시 path 지정

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,17 +1,11 @@
-import React, { useState, useCallback } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 // Category의 상태를 useState로 관리 하도록 코드 바꿈.
+// React Router Dom 을 사용했기 때문에 Route path만 간단하게 입력 함.
 const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
